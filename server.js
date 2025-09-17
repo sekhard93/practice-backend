176a6dd4010cd0153a06a35178974a3791571dd5
sekhard93 <sekhardash733@gmail.com>
2025-09-17 21:02:19 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('server is working oh yes!');
 });
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
